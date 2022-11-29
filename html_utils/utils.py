c541e204a44335ccf68e698a13e4595b31a14a3a
Aaron Garibay <ing.aarongaribay@outlook.com>
2022-11-29 13:38:56 -0800

    improving readability and adding docstrings

--- html_utils/utils.py
+++ html_utils/utils.py
@@ -1,21 +1,77 @@
 from bs4 import BeautifulSoup
+import requests
 
-def get_title(soup: BeautifulSoup):
-    # return the title of the page of empty string if not found
+
+def get_title(soup: BeautifulSoup) -> str or None:
+    """ search the title tag of a web page
+
+    Args:
+        soup (BeautifulSoup): the soup object of the web page
+
+    Returns:
+        str or None: the title of the web page or None if not found
+    """
     title = soup.find("title")
-    return title.string if title else None
+    if not title:
+        return None
+    return title.string
 
-def get_meta_name(soup: BeautifulSoup):
-    # finds the meta tag and returns the content of the name attribute or empty string if not found
-    meta_name = soup.find("meta", {"name": "description"})
-    return meta_name["content"] if meta_name else None
+
+def get_meta_name(soup: BeautifulSoup) -> str or None:
+    """ search the meta tag of a web page and returns the content of the
+    name attribute
+    
+    Args:
+        soup (BeautifulSoup): the soup object of the web page
+
+    Returns:
+        str or None: the content of the name attribute or None if not found
+    """
+
+
+    meta_name = soup.find("meta", {"name": "description"}) or {}
+    return meta_name.get("content", None)
+
 
 def get_favicon_url(soup: BeautifulSoup):
-    # finds the favicon url or empty string if not found
-    favicon_url = soup.find("link", {"rel": "icon"})
-    return favicon_url["href"] if favicon_url else None
+    """ search the favicon url of a web page
+
+    Args:
+        soup (BeautifulSoup): the soup object of the web page
+
+    Returns:
+        str or None: the favicon url of the web page or None if not found
+    """
+
+
+    favicon_url = soup.find("link", {"rel": "icon"}) or {}
+    return favicon_url.get("href", None)
+
 
 def get_first_h1_in_body(soup: BeautifulSoup):
-    # finds the first h1 in the body or empty string if not found
+    """ search the first h1 in the body of a web page
+
+    Args:
+        soup (BeautifulSoup): the soup object of the web page
+
+    Returns:
+        str or None: the first h1 in the body of the web page or None if not found
+    """
     first_h1 = soup.find("body").find("h1")
-    return first_h1.string if first_h1 else None
+    if not first_h1:
+        return None
+    return first_h1.string
+
+
+def get_web_page_content(url: str) -> str:
+    """ Given a url, do a get request and return the
+    response content compatible with BeautifulSoup
+
+    Args:
+        url (str): the url of the web page
+
+    Returns:
+        str: the response content of the web page
+    """
+    response = requests.get(url)
+    return response.content
